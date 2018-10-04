b242a7e6619b1d599bad0ae09e34aaa53422c0a4
ranjith.shivasamy <ranjith.s@cgvakindia.com>
2018-10-04 14:55:49 +0530

    Annotation added

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController 
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return new Response("<h1>Test</h1>");
+        
+    }
+}
